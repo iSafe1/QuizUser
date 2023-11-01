aa5673cff4bfd31deba458773464f041e5ae23b1
iSafe1 <146738897+iSafe1@users.noreply.github.com>
2023-11-01 23:46:43 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,331 @@
+let questions = [
+  {
+    section: "Staying Safe Online",
+    numb: 1,
+    question: "What is a good rule to stay safe from computer viruses?",
+    answer: "C) Don't open email attachments if you're not expecting them.",
+    options: [
+      "A) Download files from the Internet.",
+      "B) Open email attachments even if you're not expecting them.",
+      "C) Don't open email attachments if you're not expecting them.",
+      "D) Share your passwords with friends."
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 2,
+    question: "How can you make sure your antivirus software works well?",
+    answer: "C) Keep it up to date by installing the newest version and updating security fixes.",
+    options: [
+      "A) Never update it and keep using the old version.",
+      "B) Run it only when you remember to do so.",
+      "C) Keep it up to date by installing the newest version and updating security fixes.",
+      "D) Share your antivirus software with your friends."
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 3,
+    question: "Viruses can only be sent by E-mail.",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 4,
+    question: "Whatever you post online will stay there forever even if you delete it.",
+    answer: "True",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 5,
+    question: "You can totally share your password with your best friend, you trust them!",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 6,
+    question: "I have a digital picture of myself, and someone I met online wants to see it. Is it OK to send it to that person?",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 7,
+    question: "Is it a good idea to give your address or phone number to someone you meet online?",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 8,
+    question: "Is it okay to enter a contest on a website from a company or organization you know about that asks for your name and phone number?",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 9,
+    question: "What's a good way to create a strong password for your online accounts?",
+    answer: "A) Combine letters, numbers, and symbols, like P@ssw0rd123!",
+    options: [
+      "A) Combine letters, numbers, and symbols, like P@ssw0rd123!",
+      "B) Use your pet's name and birth date.",
+      "C) Use a password like 12345.",
+      "D) Use the word password as your password."
+    ]
+  },
+  {
+    section: "Staying Safe Online",
+    numb: 10,
+    question: "How can you keep your computer programs safe?",
+    answer: "C) Make copies of them.",
+    options: [
+      "A) Keep them away from strong magnets.",
+      "B) Put them in a safe place.",
+      "C) Make copies of them.",
+      "D) Keep them in their original boxes."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 11,
+    question: "What is cyberbullying?",
+    answer: "C) Using the internet to harm or harass others.",
+    options: [
+      "A) A way to make new friends online.",
+      "B) A fun online game.",
+      "C) Using the internet to harm or harass others.",
+      "D) Sharing positive messages online."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 12,
+    question: "How should you respond if someone is mean to you or someone else online?",
+    answer: "C) Report it to a trusted adult or a platform's reporting system.",
+    options: [
+      "A) Be mean back to them.",
+      "B) Ignore it and don't tell anyone.",
+      "C) Report it to a trusted adult or a platform's reporting system.",
+      "D) Encourage others to be mean too."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 13,
+    question: "Cybercriminals only target large companies. True or False?",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 14,
+    question: "If your friend makes a hurtful video about you online, it's a good idea to make a hurtful video in return.",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 15,
+    question: "What should you do if you see a mean comment about someone on a social media post or chat?",
+    answer: "C) Stand up for the person being targeted and report the mean commentness helps create a positive and safe online environment.",
+    options: [
+      "A) Share it with your friends to show how mean people can be.",
+      "B) Ignore it and keep scrolling.",
+      "C) Stand up for the person being targeted and report the mean commentness helps create a positive and safe online environment.",
+      "D) Add your own mean comment to fit in."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 16,
+    question: "What should you do if you are being cyber-bullied?",
+    answer: "A) All of the above.",
+    options: [
+      "A) All of the above.",
+      "B) Do not respond to the cyberbully.",
+      "C) Tell a trusted adult.",
+      "D) Block them if you can."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 17,
+    question: "Hacking into someone's computer is illegal",
+    answer: "YES",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 18,
+    question: "Anything you say or type online becomes public knowledge and stays online forever...even if you THINK you deleted it.",
+    answer: "YES",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 19,
+    question: "What should you do if you accidentally come across inappropriate or harmful content involving kids online?",
+    answer: "B) Report it to the appropriate authorities.",
+    options: [
+      "A) Share it with friends to raise awareness.",
+      "B) Report it to the appropriate authorities.",
+      "C) Ignore it and continue browsing.",
+      "D) Download and keep it as evidence."
+    ]
+  },
+  {
+    section: "Cyber Bullying Awareness",
+    numb: 20,
+    question: "If someone is being cyberbullied, what can you do to help them?",
+    answer: "D) Stand up for the person being bullied and report the cyberbullying.",
+    options: [
+      "A) Ignore them and let them handle it themselves.",
+      "B) Share the cyberbullying messages with others for fun.",
+      "C) Join in and make fun of the person being bullied.",
+      "D) Stand up for the person being bullied and report the cyberbullying."
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 21,
+    question: "Below are the tips for good password management except:",
+    answer: "A) Use the same password over and over again.",
+    options: [
+      "A) Use the same password over and over again.",
+      "B) Avoid using simple adjacent keyboard combination as your password.",
+      "C) Change password often.",
+      "D) Make difficult to guess password."
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 22,
+    question: "Which one of the following describes spam?",
+    answer: "A) Sending lots of unwanted messages.",
+    options: [
+      "A) Sending lots of unwanted messages.",
+      "B) A delicious type of food.",
+      "C) A type of computer game.",
+      "D) A friendly email from a family member."
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 23,
+    question: "To use the new social media app that requires users to be 18 years or older, is it okay to lie about your age and sign up yourself.",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 24,
+    question: "If you receive an email claiming you've won a cash prize and it suggests clicking on the attachment to claim it, you should open the attachment.",
+    answer: "False",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 25,
+    question: "When creating a password for a new email account, it's a good idea to use a phrase that includes numbers and symbols.",
+    answer: "True",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 26,
+    question: "The best passwords include your name, age, or birthday.",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 27,
+    question: "Is it a good idea to share your phone or school-related passwords with anyone, even your friends, online or offline?",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 28,
+    question: "Is it safe to use the same password for multiple accounts? ",
+    answer: "NO",
+    options: [
+      "YES",
+      "NO"
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 29,
+    question: "Which of the following is considered unsafe online behavior?",
+    answer: "D) Be friend with the people, you don't know.",
+    options: [
+      "A) Watching YouTube.",
+      "B) Typing A Fairy Tale.",
+      "C) Playing Games.",
+      "D) Be friend with the people, you don't know."
+    ]
+  },
+  {
+    section: "Password Safety",
+    numb: 30,
+    question: "To make a strong password, you need at least how many characters?",
+    answer: "B) 8.",
+    options: [
+      "A) 6.",
+      "B) 8.",
+      "C) 68.",
+      "D) 12."
+    ]
+  },
+  // Add more questions for different sections
+];
